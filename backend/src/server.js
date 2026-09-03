29f5c5b6614f9e418bd101a52285cd3ef248e2ba
arunpravin125 <162274540+arunpravin125@users.noreply.github.com>
2026-09-03 21:27:50 +0530

    user routes added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectMongoose } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
 import { ENV } from "./config/env.js";
+import cors from "cors";
+import { userRoutes } from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
 const PORT = ENV.PORT || 3011;
 
 app.get("/", (req, res) => res.send("Hellow from Server"));
+
+app.use("/api/users", userRoutes);
 
 const startServer = async () => {
   await connectMongoose();
